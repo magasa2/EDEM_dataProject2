41644af76fa04dded05156bfeaef89639f15bae1
Maria Garcia Sancho <garciasancho17@gmail.com>
2023-02-05 19:43:32 +0100

    832

--- otas_cosas/comandos.py
+++ otas_cosas/comandos.py
@@ -5,11 +5,20 @@ Bucket_name = edemproject2
 gcloud builds submit --tag 'gcr.io/edemproject2/dataflow/edem:latest' .
 
 
+#Create Dataflow Flex Template from Image:
 gcloud dataflow flex-template build "gs://edemproject2/dataflowtemplate.json" \
-  --image "gcr.io/edemproject2/dataflow/edem:latest" \
-  --sdk-language "PYTHON"
+    --image 'gcr.io/edemproject2/dataflow/edem:latest' \
+    --sdk-language "PYTHON" \
+    --metadata-file "schemas/metadata.json"
 
-
+#Run Dataflow Flex Template:
+gcloud dataflow flex-template run "edem-dataflow-job-docker" \
+    --template-file-gcs-location "gs://edemproject2/dataflowtemplate.json" \
+    --parameters project_id="edemproject2" \
+    --parameters input_subscription="iotToBigQuery-sub" \
+    --parameters output_topic="iotToBigQuery" \
+    --parameters output_bigquery="edemDataset.edemTable2" \
+    --region europe-west1
 
  #Para iniciar DataFlow
 python edem_dataflow_streaming.py \
